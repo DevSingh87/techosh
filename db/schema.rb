0fc26f2c63d395cb8e6c9b2ca910bd5a2ead98f1
TechieDev <techiedev1987@gmail.com>
2015-10-07 14:13:30 +0530

    Gemfile updated.

--- db/schema.rb
+++ db/schema.rb
@@ -9,132 +9,132 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150925104932) do
+ActiveRecord::Schema.define(version: 20150925104932) do
 
-  create_table "articles", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "categories", :force => true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "categories", ["slug"], :name => "index_categories_on_slug"
+  add_index "categories", ["slug"], name: "index_categories_on_slug", using: :btree
 
-  create_table "images", :force => true do |t|
-    t.integer  "tutorial_id"
-    t.string   "image"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+  create_table "images", force: :cascade do |t|
+    t.integer  "tutorial_id", limit: 4
+    t.string   "image",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "images", ["tutorial_id"], :name => "index_images_on_tutorial_id"
+  add_index "images", ["tutorial_id"], name: "index_images_on_tutorial_id", using: :btree
 
-  create_table "impressions", :force => true do |t|
-    t.string   "impressionable_type"
-    t.integer  "impressionable_id"
-    t.integer  "user_id"
-    t.string   "controller_name"
-    t.string   "action_name"
-    t.string   "view_name"
-    t.string   "request_hash"
-    t.string   "ip_address"
-    t.string   "session_hash"
-    t.text     "message"
-    t.text     "referrer"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+  create_table "impressions", force: :cascade do |t|
+    t.string   "impressionable_type", limit: 255
+    t.integer  "impressionable_id",   limit: 4
+    t.integer  "user_id",             limit: 4
+    t.string   "controller_name",     limit: 255
+    t.string   "action_name",         limit: 255
+    t.string   "view_name",           limit: 255
+    t.string   "request_hash",        limit: 255
+    t.string   "ip_address",          limit: 255
+    t.string   "session_hash",        limit: 255
+    t.text     "message",             limit: 65535
+    t.text     "referrer",            limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "impressions", ["controller_name", "action_name", "ip_address"], :name => "controlleraction_ip_index"
-  add_index "impressions", ["controller_name", "action_name", "request_hash"], :name => "controlleraction_request_index"
-  add_index "impressions", ["controller_name", "action_name", "session_hash"], :name => "controlleraction_session_index"
-  add_index "impressions", ["impressionable_type", "impressionable_id", "ip_address"], :name => "poly_ip_index"
-  add_index "impressions", ["impressionable_type", "impressionable_id", "request_hash"], :name => "poly_request_index"
-  add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], :name => "poly_session_index"
-  add_index "impressions", ["impressionable_type", "message", "impressionable_id"], :name => "impressionable_type_message_index", :length => {"impressionable_type"=>nil, "message"=>255, "impressionable_id"=>nil}
-  add_index "impressions", ["user_id"], :name => "index_impressions_on_user_id"
+  add_index "impressions", ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index", using: :btree
+  add_index "impressions", ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index", using: :btree
+  add_index "impressions", ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index", using: :btree
+  add_index "impressions", ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index", using: :btree
+  add_index "impressions", ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index", using: :btree
+  add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
+  add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", length: {"impressionable_type"=>nil, "message"=>255, "impressionable_id"=>nil}, using: :btree
+  add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
 
-  create_table "subjects", :force => true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "subjects", ["slug"], :name => "index_subjects_on_slug"
+  add_index "subjects", ["slug"], name: "index_subjects_on_slug", using: :btree
 
-  create_table "taggings", :force => true do |t|
-    t.integer  "tutorial_id"
-    t.integer  "tag_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tutorial_id", limit: 4
+    t.integer  "tag_id",      limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "tutorials", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "slug"
-    t.integer  "category_id"
-    t.integer  "university_id"
-    t.integer  "subject_id"
+  create_table "tutorials", force: :cascade do |t|
+    t.string   "title",         limit: 255
+    t.text     "description",   limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug",          limit: 255
+    t.integer  "category_id",   limit: 4
+    t.integer  "university_id", limit: 4
+    t.integer  "subject_id",    limit: 4
   end
 
-  add_index "tutorials", ["slug"], :name => "index_tutorials_on_slug"
+  add_index "tutorials", ["slug"], name: "index_tutorials_on_slug", using: :btree
 
-  create_table "universities", :force => true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "universities", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "universities", ["slug"], :name => "index_universities_on_slug"
+  add_index "universities", ["slug"], name: "index_universities_on_slug", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "videos", :force => true do |t|
-    t.integer  "tutorial_id"
-    t.string   "title"
-    t.string   "video_url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "slug"
+  create_table "videos", force: :cascade do |t|
+    t.integer  "tutorial_id", limit: 4
+    t.string   "title",       limit: 255
+    t.string   "video_url",   limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug",        limit: 255
   end
 
-  add_index "videos", ["slug"], :name => "index_videos_on_slug"
-  add_index "videos", ["tutorial_id"], :name => "index_videos_on_tutorial_id"
+  add_index "videos", ["slug"], name: "index_videos_on_slug", using: :btree
+  add_index "videos", ["tutorial_id"], name: "index_videos_on_tutorial_id", using: :btree
 
 end
